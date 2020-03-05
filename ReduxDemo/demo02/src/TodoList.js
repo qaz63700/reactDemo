9d4c4fddd540a04b042b918672e72942bc767830
qaz63700 <641838487@qq.com>
2020-03-05 23:07:58 +0800

    tj

--- ReduxDemo/demo02/src/TodoList.js
+++ ReduxDemo/demo02/src/TodoList.js
@@ -1,23 +1,45 @@
 import React, { Component } from 'react';
-import store from './store'
+// import store from './store'
+import { connect } from 'react-redux';
 class TodoList  extends Component {
-        constructor(props) {
-                super(props);
-                this.state=store.getState()
-        }
+
         render() { 
                 return ( 
                         <div>
-                                <input value={this.state.inputValue} />
-                                <button>提交</button>
+                                <input value={this.props.inputValue}  onChange={this.props.inputChange}/>
+                                <button onClick={this.props.clickButton}>提交</button>
                                 <ul>
-                                        <li>
-                                                JSPANG
-                                        </li>
+                                      {
+                                              this.props.list.map((item,index)=>{
+                                                return (<li key={index}>{item}</li>)
+                                            })
+                                      } 
                                 </ul>
                         </div>
                  );
         }
 }
- 
-export default TodoList;
+const stateToProps=(state)=>{
+	return{
+                inputValue:state.inputValue,
+                list:state.list
+	}
+}
+
+const dispatchToProps = (dispatch) =>{
+        return {
+                inputChange(e){
+                        let action = {
+                                type:'change_input',
+                                value:e.target.value
+                        }
+                        dispatch(action)
+                },
+                clickButton(){
+                        // console.log(111)
+                        let action = { type:'add_item' }
+                        dispatch(action)
+                }
+        }
+    }
+export default connect(stateToProps,dispatchToProps)(TodoList);
