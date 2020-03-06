ec102cf30b19b2a5072521f435fcafcdb72b0dec
陈儒钢 <641838487@qq.com>
2020-03-06 15:02:53 +0800

    tj

--- ReduxDemo/demo02/src/TodoList.js
+++ ReduxDemo/demo02/src/TodoList.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
 // import store from './store'
 import { connect } from 'react-redux';
-class TodoList  extends Component {
-
-        render() { 
-                return ( 
-                        <div>
-                                <input value={this.props.inputValue}  onChange={this.props.inputChange}/>
-                                <button onClick={this.props.clickButton}>提交</button>
-                                <ul>
-                                      {
-                                              this.props.list.map((item,index)=>{
-                                                return (<li key={index}>{item}</li>)
-                                            })
-                                      } 
-                                </ul>
-                        </div>
-                 );
-        }
+const TodoList = (props)=>{
+        let {inputValue,inputChange,clickButton,list}=props
+        return ( 
+                <div>
+                        <input value={inputValue}  onChange={inputChange}/>
+                        <button onClick={clickButton}>提交</button>
+                        <ul>
+                                {
+                                        list.map((item,index)=>{
+                                        return (<li key={index}>{item}</li>)
+                                        })
+                                } 
+                        </ul>
+                </div>
+                );
 }
 const stateToProps=(state)=>{
 	return{
