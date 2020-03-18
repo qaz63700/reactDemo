ace4a488d1b663f737c5785726439321cbae1014
qaz63700 <641838487@qq.com>
2020-03-18 23:13:58 +0800

    5tj

--- NextDemo/next-create/pages/jspang.js
+++ NextDemo/next-create/pages/jspang.js
@@ -1,5 +1,20 @@
-function Jspang(){
-return (<button>技术胖</button>)
-}
+import React, {useState} from 'react'
 
-export default  Jspang;
+function Jspang(){
+        const [color,setColor] = useState('blue')
+const changeColor=()=>{
+        setColor(color=='blue'?'red':'blue')
+}
+        return(
+                <>
+                        <div>技术胖免费前端教程</div>
+                        <div><button onClick={changeColor}>改变颜色</button></div>
+                        <style jsx>
+                        {`
+                                div { color:${color};}
+                                `}
+                        </style>
+                </>
+        )
+}
+export default Jspang
---
+++ NextDemo/next-create/pages/jspangB.js
@@ -0,0 +1,16 @@
+function Jspang(){
+    return (
+        <>
+            <div>技术胖免费前端教程</div>
+            <div className="jspang">技术胖免费前端教程</div>
+
+            <style jsx>
+                {`
+                    div { color:blue;}
+                    .jspang {color:red;}
+                `}
+            </style>
+        </>
+    )
+}
+export default Jspang
