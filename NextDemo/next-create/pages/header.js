68e956ed52dbb30ffa4f3f142a492eebf0650740
qaz63700 <641838487@qq.com>
2020-03-19 23:13:41 +0800

    TJ

---
+++ NextDemo/next-create/pages/header.js
@@ -0,0 +1,13 @@
+import Myheader from '../components/myheader'
+import {Button} from 'antd'
+function Header(){ 
+    return (
+        <>
+            <Myheader />
+            <div>JSPang.com</div>
+            <div><Button>我是按钮</Button></div>
+
+        </> 
+    )
+}
+export default Header
